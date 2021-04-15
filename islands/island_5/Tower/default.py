2ea71a027d107bce025903d2274609d21e82ed4b
Neko1993 <koshka1993@xakep.ru>
2021-04-15 13:17:02 +0300

    initial

--- islands/island_5/Tower/default.py
+++
@@ -1,5 +0,0 @@
-def build_tower():
-    for height in range(0, 15):
-        agent.move("up")
-        draw_square( )
-        # Complete the line above
--- islands/island_5/Wire/default.py
+++
@@ -1,5 +0,0 @@
-def on_player_travelled(location, mode, distance):
-    loc = correct_location(location)
-    agent.teleport(loc)
-    agent.     ( , "    ")
-    # Finish the line above.
